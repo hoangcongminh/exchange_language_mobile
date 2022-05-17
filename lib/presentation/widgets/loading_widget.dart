81186665e089c42220f4440aebe1abc28b7a2c3e
hoangcongminh <congminh2320@gmail.com>
2022-05-17 12:01:18 +0700

    update loadingwidget with spinkit package

--- lib/presentation/widgets/loading_widget.dart
+++ lib/presentation/widgets/loading_widget.dart
@@ -1,14 +1,28 @@
-import 'package:flutter/cupertino.dart';
+import 'package:exchange_language_mobile/presentation/theme/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class LoadingWidget extends StatelessWidget {
   const LoadingWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const AbsorbPointer(
+    return AbsorbPointer(
       child: Center(
-        key: ValueKey('home_LoadingWidget'),
-        child: CupertinoActivityIndicator(),
+        key: const ValueKey('home_LoadingWidget'),
+        // child: CupertinoActivityIndicator(),
+        child: SpinKitThreeInOut(
+          itemBuilder: (BuildContext context, int index) {
+            return DecoratedBox(
+              decoration: BoxDecoration(
+                color: index.isEven
+                    ? AppColors.primaryColor
+                    : AppColors.primaryColor.withOpacity(0.5),
+                shape: BoxShape.circle,
+              ),
+            );
+          },
+        ),
       ),
     );
   }
